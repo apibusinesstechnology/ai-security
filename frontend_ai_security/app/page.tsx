dbb3d0f856a6c4f19e846d824a98b5f552d04336
Pierre Richard Saint Louis <saintlouispierrerichard6@gmail.com>
2026-07-25 19:37:32 -0400

    Developpement de la page page splash.

---
+++ frontend_ai_security/app/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+import PageSplash from "./components/Page_splash";
+
+export default function Home() {
+  return (
+  
+  <div>
+  
+    <PageSplash/>
+    
+    </div>
+    
+  );
+}
+
+
+
+
