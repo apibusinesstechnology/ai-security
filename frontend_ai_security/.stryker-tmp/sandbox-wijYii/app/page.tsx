d4848013c76c5ddefa9ed59849f0f6cfd946229e
Pierre Richard Saint Louis <saintlouispierrerichard6@gmail.com>
2026-09-01 20:28:51 -0400

    Ajout et configuration des test.

---
+++ frontend_ai_security/.stryker-tmp/sandbox-wijYii/app/page.tsx
@@ -0,0 +1,19 @@
+// @ts-nocheck
+"use client";
+import PageSplash from "./components/Page_splash";
+
+export default function Home() {
+  return (
+  
+  <div>
+  
+    <PageSplash/>
+    
+    </div>
+    
+  );
+}
+
+
+
+
---
+++ frontend_ai_security/.stryker-tmp/sandbox-wijYii/jest.config.js
@@ -0,0 +1,24 @@
+// @ts-nocheck
+module.exports = {
+  roots: ["<rootDir>/../dossier_test"],
+
+  testEnvironment: "jest-environment-jsdom",
+  setupFilesAfterEnv: ["<rootDir>/../jest.setup.js"],
+
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/$1"
+  },
+
+  transform: {
+    "^.+\\.(ts|tsx)$": [
+      "ts-jest",
+      { tsconfig: "<rootDir>/../tsconfig.jest.json" }
+    ]
+  },
+
+  testMatch: [
+    "<rootDir>/../dossier_test/**/*.test.ts",
+    "<rootDir>/../dossier_test/**/*.test.tsx"
+  ]
+};
+
